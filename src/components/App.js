57b0dac24d1092d2e39f190606e24f4de2114499
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2023-01-25 19:32:28 +0200

    Work_-11

--- src/components/App.js
+++ src/components/App.js
@@ -40,7 +40,8 @@ export const App = () => {
                         <Route
                             path="/register"
                             element={
-                                <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />
+                                // <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />
+                                <RestrictedRoute redirectTo="/login" component={<RegisterPage />} />
                             }
                         />
                         <Route
