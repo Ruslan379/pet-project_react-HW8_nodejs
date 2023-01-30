1d451dbaead4e790e3a33d830a7f4bc8bb424a83
Ruslan379 <arnov@i.ua>
2023-01-30 15:52:26 +0200

    Final_draft_ALL TASK__40

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -7,9 +7,9 @@ import { toast } from 'react-toastify';
 
 
 
-//! axios defaults baseURL
-axios.defaults.baseURL = 'http://localhost:3000/api'; //??
-// axios.defaults.baseURL = 'http://192.168.99.100:3000/api';
+//! axios defaults baseURL (возможно, это не надо, т.к. уже есть в authOperations.js)
+// axios.defaults.baseURL = 'http://localhost:3000/api'; //??
+axios.defaults.baseURL = 'http://192.168.99.100:3000/api';
 
 
 //! GET @ /contacts
