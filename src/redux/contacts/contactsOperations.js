19f8949d811d9d8b7346be7e345a65abc7dff791
Ruslan379 <arnov@i.ua>
2023-01-30 14:38:31 +0200

    Final_draft_ALL TASK__38

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -8,7 +8,8 @@ import { toast } from 'react-toastify';
 
 
 //! axios defaults baseURL
-axios.defaults.baseURL = 'http://localhost:3000/api'; //??
+// axios.defaults.baseURL = 'http://localhost:3000/api'; //??
+axios.defaults.baseURL = 'http://192.168.99.100:3000/api';
 
 
 //! GET @ /contacts
