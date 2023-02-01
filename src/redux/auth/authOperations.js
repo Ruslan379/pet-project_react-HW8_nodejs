cd740131dd4eab638d00db7cd059ecadb091ef63
Ruslan379 <arnov@i.ua>
2023-02-01 03:22:23 +0200

    Final_draft_ALL TASK+Render__41

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -10,7 +10,8 @@ import { toast } from 'react-toastify';
 //! axios defaults baseURL
 // axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 // axios.defaults.baseURL = 'http://localhost:3000/api';
-axios.defaults.baseURL = 'http://192.168.99.100:3000/api';
+// axios.defaults.baseURL = 'http://192.168.99.100:3000/api';
+axios.defaults.baseURL = 'http://contact-book-backend52.onrender.com';
 
 //! Utility to add JWT
 const setAuthHeader = token => {
